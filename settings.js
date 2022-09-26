a49c1e2e534a4b8b3247116a2dea7c8256abfb79
Bihari-back <114386902+Bihari-back@users.noreply.github.com>
2022-09-27 01:44:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,15 +54,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['97696105222'] //ur owner number
+global.vcardowner = ['15817042904'] //ur owner number
 global.ownername = "🦄king bihari" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['97696105222']
-global.ownertag = '97696105222' //ur tag number
+global.owner = ['15817042904']
+global.ownertag = '15817042904' //ur tag number
 global.botname = 'bihari raja' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['97696105222'] //ur premium numbers
+global.premium = ['15817042904'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
